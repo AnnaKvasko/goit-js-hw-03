2c602c909e6bb5ab523c9259030f58936635ecfc
Kvasko Anna <kvasko.anna@ukr.net>
2025-05-24 00:25:35 +0300

    hw-3

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function makeArray (firstArray, secondArray, maxLength) {
+    const resultArray = [];
+    const firstArrayLength = firstArray.Length;
+    const secondArrayLength = secondArray.Length;
+    const minLength = Math.min(firstArray.Length, secondArray.Length);
+    const maxLength = Math.max(firstArray.Length, secondArray.Length);
+
+    for (let i=0; i < maxLenght; i++) {
+        if (i < minLenght) 
+
+    }
+
+
+
+}
